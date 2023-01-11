33eefd5c7b50e8954457315b0938635cf42346ad
ehumlen <ehumlen@protonmail.com>
2023-01-11 23:36:49 +0100

    Updated to include Correlation Ranker for ROI

--- Research Code/r001 - ROx Impact on Returns (WIP).py
+++ Research Code/r001 - ROx Impact on Returns (WIP).py
@@ -10,6 +10,7 @@ have on future performances of stocks.
 #%% Package Installs
 import pandas as pd
 import yfinance as yf
+import numpy as np
 from datetime import datetime, timedelta
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -31,8 +32,6 @@ warnings.filterwarnings("ignore")
 
 df_tickers = pd.read_csv("../Research Resources/Tickers.csv") #1036 Entries
 df_tickers = df_tickers.drop_duplicates(subset=["comp_name"])
-tickerlist = df_tickers["ticker"].to_list()[2724:]
-companylist = df_tickers["comp_name"][2724:]
 start_date = '2010-01-01'
 end_date = datetime.now()+timedelta(days=-730)
 list_roi = []
@@ -52,6 +51,10 @@ df_tickers = df_tickers[(df_tickers['country_code'] == country_code)]
 # Remove Microcaps (Below 100M )
 df_tickers = df_tickers[(df_tickers['market_val'] >= 100)]
 # 2594 Left
+
+tickerlist = df_tickers["ticker"].to_list()
+companylist = df_tickers["comp_name"].to_list()
+
 
 #%% YFinance Equity Price Collection
 
@@ -74,15 +77,17 @@ for i in tqdm(range(len(tickerlist))):
   list_prices.append(df_price)
   
   # Naptime
-  time.sleep(sleeptime)
+ # time.sleep(sleeptime)
   
   
   
 df_prices = pd.concat(list_prices, axis = 0) 
+df_prices = df_prices.dropna()
 
 #%% ROI Data Collection
 
 # Loop through list elements to collect data
+print("Be patient, this will take some time...")
 for i in tqdm(range(len(tickerlist))):
   # Collect Ticker
   ticker = tickerlist[i]
@@ -91,16 +96,19 @@ for i in tqdm(range(len(tickerlist))):
   # Get ROI-table from Macrotrends
   roi = "{base}{ticker}/{company}/roi".format(ticker=ticker,company=company,base=mt)
   # Remake into a dataframe
-  df_roi = pd.read_html(roi)[0]
-  df_roi["Ticker"] = ticker
-  df_roi.columns= ['Date','Net Income','Assets','ROI','Ticker']
-  df_roi['Date'] = df_roi['Date'].astype('datetime64')
-  df_roi = df_roi[(df_roi['Date'] >= start_date) & (df_roi['Date']< end_date)]
-  list_roi.append(df_roi)
-  
-  # Naptime
-  sleeptime = random.randint(0,15)
-  time.sleep(sleeptime)
+  try:
+      df_roi = pd.read_html(roi)[0]
+      df_roi["Ticker"] = ticker
+      df_roi.columns= ['Date','Net Income','Assets','ROI','Ticker']
+      df_roi['Date'] = df_roi['Date'].astype('datetime64')
+      df_roi = df_roi[(df_roi['Date'] >= start_date) & (df_roi['Date']< end_date)]
+      list_roi.append(df_roi)
+      # Naptime
+      sleeptime = random.randint(0,10)
+      time.sleep(sleeptime)
+  except:
+      pass
+
 
 
   # Append to list of dataframes
@@ -134,14 +142,18 @@ df_base = pd.merge(df_base, df_prices, how='left', left_on=['1 Year','Ticker'], 
 df_base = pd.merge(df_base, df_prices, how='left', left_on=['2 Years','Ticker'], right_on=['Date','Ticker'], suffixes= ('','_2y'))
 #df_base = df_base.drop(['Date_6m','Date_1y','Date_2y'], axis = 1)
 
-#%% Add SPY prices to table
+#%% Widen Table
 
+# Add SPY
 df_base = pd.merge(df_base,df_spy, how = 'left', left_on='Date', right_on = 'Date', suffixes = ('','_2'))
 df_base = pd.merge(df_base,df_spy, how = 'left', left_on='Date_6m', right_on = 'Date', suffixes = ('','_6m'))
 df_base = pd.merge(df_base,df_spy, how = 'left', left_on='Date_1y', right_on = 'Date', suffixes = ('','_1y'))
 df_base = pd.merge(df_base,df_spy, how = 'left', left_on='Date_2y', right_on = 'Date', suffixes = ('','_2y'))
-
 df_base = df_base.drop(['Date_6m','Date_1y','Date_2y', 'Ticker_2', 'Ticker_6m', 'Ticker_1y', 'Ticker_2y'], axis = 1)
+
+# Add Original Data
+df_base = pd.merge(df_base, df_tickers, how='left', left_on='Ticker', right_on='ticker')
+df_base = df_base.drop(["comp_name","pe_ratio_12m","price_per_sales","div_yield","held_by_insiders_pct","held_by_institutions_pct","link"], axis =1)
 
 
 #%% Calculate returns
@@ -173,31 +185,36 @@ THIS IS THE END OF DATA AGGREGATION AND BEGINNING OF DATA ANALYSIS
 
                  ═════════•°• ⚠ •°•═════════                
 """
+#%%  Get Vars
+print(df_base.sector.unique())
+
+
 #%% Normalize Dataset
 
-df_base_filtered = df_base.dropna()
-df_base_filtered.info()
-
+df_base_filtered = df_base.replace([np.inf, -np.inf], np.nan).dropna(axis=0)
 
 # Set target
 variable = "ROI"
-target = "Return_6m"
-comparison = "Index_2y"
+target = "Delta_6m"
 
-
+# Filter Sector
+# sectors = ["Construction"]
+# df_base_filtered = df_base_filtered[df_base_filtered["sector"].isin(sectors)]
 
 # Remove Outlier Dates
-df_base_filtered = df_base[(df_base["Date"] >= start_date)]
+df_base_filtered = df_base_filtered[(df_base_filtered["Date"] >= start_date)]
 
 # Remove Outlier ROAs
-roa_low = df_base_filtered[variable].quantile(0.03)
-roa_high  = df_base_filtered[variable].quantile(0.97)
-df_base_filtered = df_base_filtered[(df_base_filtered[variable] < roa_high) & (df_base_filtered[variable] > roa_low)]
+roi_low = df_base_filtered[variable].quantile(0.025)
+roi_high  = df_base_filtered[variable].quantile(0.975)
+df_base_filtered = df_base_filtered[(df_base_filtered[variable] < roi_high) & (df_base_filtered[variable] > roi_low)]
 
 # Remove Outlier Returns
-ret_low = df_base_filtered[target].quantile(0.03)
-ret_high  = df_base_filtered[target].quantile(0.97)
+ret_low = df_base_filtered[target].quantile(0.025)
+ret_high  = df_base_filtered[target].quantile(0.975)
 df_base_filtered = df_base_filtered[(df_base_filtered[target] < ret_high) & (df_base_filtered[target] > ret_low)]
+
+df_base_filtered.info()
 
  #%% Plots
 
@@ -213,7 +230,7 @@ plt.show()
 
 # Line Chart with average Performance per Year
 plt.figure(dpi=1200)
-graph = sns.lineplot(data=df_base, x="Date", y=target, ci =95)
+graph = sns.lineplot(data=df_base_filtered, x="Date", y=target, ci =95)
 graph.axhline(y=0, color = 'black', linestyle = '--', lw = 1) # Net Zero
 graph.set_title("Relative Return vs Market avg., {}".format(target))
 graph.set_ylabel("% Return, 1Y Later")
@@ -223,12 +240,37 @@ plt.show()
 # Line Chart with ROA (/w Confidence Interval) and Relative Market Outperformance
 plt.figure(dpi=1200)
 sns.lineplot(data=df_base_filtered, x=variable, y=target, ci=95)
-plt.axhline(y=0, color = 'black', linestyle = '--', lw = 1) # Net Zero
-#mean_y = df_base_filtered["Delta_1y"].mean()
-# plt.axhline(y=mean_y, color = 'orange', linestyle = '--', lw = 1) # Dataset Mean
+plt.axhline(y=0, color = 'black', linestyle = '--', lw = .3) # Net Zero
+plt.axvline(x=0, color = 'black', linestyle = '--', lw = .3) # Net Zero
 plt.title("ROA and Performance Distribution, {}".format(target))
 plt.ylabel("Return vs S&P500, 1Y later")
 
 
+#%% Correlation Ranker
+
+from scipy.stats import pearsonr
+
+corr, _ = pearsonr(df_base_filtered["ROI"],df_base_filtered["Return_6m"])
+print("Pearsons Correlation (6m return): %.3f" % corr)
+
+corr, _ = pearsonr(df_base_filtered["ROI"],df_base_filtered["Return_1y"])
+print("Pearsons Correlation (1y return): %.3f" % corr)
+
+corr, _ = pearsonr(df_base_filtered["ROI"],df_base_filtered["Return_2y"])
+print("Pearsons Correlation (2y return): %.3f" % corr)
 
 
+#%%
+sectorlist = df_base_filtered.sector.unique().to_list()
+    
+for i in tqdm(range(len(sectorlist))):
+    sector = sectorlist[i]
+    corr_r6m, _ = pearsonr(df_base_filtered["ROI"],df_base_filtered["Return_6m"])
+    corr_r1y, _ = pearsonr(df_base_filtered["ROI"],df_base_filtered["Return_1y"])
+    corr_r2y, _ = pearsonr(df_base_filtered["ROI"],df_base_filtered["Return_2y"])
+    corr_d6m, _ = pearsonr(df_base_filtered["ROI"],df_base_filtered["Delta_6m"])
+    corr_d1y, _ = pearsonr(df_base_filtered["ROI"],df_base_filtered["Delta_1y"])
+    corr_d2y, _ = pearsonr(df_base_filtered["ROI"],df_base_filtered["Delta_2y"])
+
+
+df_base.to_csv(r'C:\Users\eirik\OneDrive\Desktop\base.csv', index=False)
